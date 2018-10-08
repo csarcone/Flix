d9adbbbcd61333e48fd2d03df7c8663a423e6ca6
Christina Sarcone <christina12245@gmail.com>
2018-10-08 12:30:29 -0400

    Updated files for week 2 project

---
+++ Views/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Christina S. on 10/8/18.
+//  Copyright © 2018 Christina S. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
